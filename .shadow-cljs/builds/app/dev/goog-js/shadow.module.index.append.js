bd28334064233be13be46e34c2dce2ba7c4219db
Jace Rockman <jacemaverick5@gmail.com>
2025-12-09 02:34:43 -0600

    Revert "Initial commit: mostly fixing routing"
    This reverts commit cf8f6fc84c538deae49f69c4c41172d28cd4939e.

--- .shadow-cljs/builds/app/dev/goog-js/shadow.module.index.append.js
+++
@@ -1,1 +0,0 @@
-["^ ","~:resource-id",["~:shadow.build.modules/append","~:index"],"~:source","\ntry { main.init(); } catch (e) { console.error(\"An error occurred when calling (main/init)\"); console.error(e); }","~:js","\ntry { main.init(); } catch (e) { console.error(\"An error occurred when calling (main/init)\"); console.error(e); }"]
--- expo-build/shadow.js.shim.module$$expo$vector_icons.js
+++
@@ -1,6 +0,0 @@
-goog.provide("shadow.js.shim.module$$expo$vector_icons");
-goog.provide("module$shadow_js_shim_module$$expo$vector_icons");
-shadow.js.shim.module$$expo$vector_icons = shadow.js.nativeProvides["@expo/vector-icons"] = require("@expo/vector-icons");
-module$shadow_js_shim_module$$expo$vector_icons.default = shadow.js.shim.module$$expo$vector_icons;
-
-//# sourceMappingURL=shadow.js.shim.module$$expo$vector_icons.js.map
